ef042539ccd62fdfc113bc1bb1a9ea79316bbff4
ADITYA KADAM <adityakadam04082006@gmail.com>
2026-02-04 00:26:47 +0530

    Add files via upload

---
+++ Lecture 7 python/Pratice Question Lecture 7/2.Search for character.py
@@ -0,0 +1,9 @@
+# search if the word learning exists in the file or not 
+
+word = "learning"
+with open("pratice.txt","r") as f:
+    data = f.read()
+if(data.find(word) != -1 ):
+    print("found")
+else:
+   print("not found")   
