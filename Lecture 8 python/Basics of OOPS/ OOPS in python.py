8d1a475cded5d45fcc051123ba3fec8ae7e5c162
ADITYA KADAM <adityakadam04082006@gmail.com>
2026-02-06 22:44:18 +0530

    Add files via upload

---
+++ Lecture 8 python/Basics of OOPS/ OOPS in python.py
@@ -0,0 +1,28 @@
+# OOPS -- Objects Oriented programming
+
+# To map with real world scenarios, we started using objects in code. 
+# This is called Objects Oriented programming.
+
+# _______________________________________________________________________________________________
+# _______________________________________________________________________________________________
+# CLASS & OBJECTS in python
+
+# class is a blueprint for creating objects.
+
+# -----------------this is an overview-------------------
+# creating class
+
+class students:
+     name = "Aditya kadam"
+
+# creating object(instance)
+
+s1 = students()
+print(s1.name)
+
+
+               
+    
+    
+
+
