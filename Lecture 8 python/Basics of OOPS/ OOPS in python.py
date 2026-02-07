b3dcde4aac6cd7336c6fed60113c54b0581f4c93
ADITYA KADAM <adityakadam04082006@gmail.com>
2026-02-07 22:18:47 +0530

    Update  OOPS in python.py

--- Lecture 8 python/Basics of OOPS/ OOPS in python.py
+++ Lecture 8 python/Basics of OOPS/ OOPS in python.py
@@ -10,19 +10,125 @@
 # class is a blueprint for creating objects.
 
 # -----------------this is an overview-------------------
-# creating class
+# creating class **
 
 class students:
      name = "Aditya kadam"
 
-# creating object(instance)
+# creating object(instance) **
 
 s1 = students()
 print(s1.name)
 
+# ________________________________________________________________________________________________
+# ________________________________________________________________________________________________
+# _ _inti_ _ Function
 
-               
+# constructor **
+
+# All classes have a function called _inti_(), which is always executrd when the class
+# is being initiated.
+# means this function works when its time for object creation.
+# even ifyou have not created one init function the computer will always make it for you
+# and execute .
+
+# -----------------this is an overview-------------------
+# This is a parameterized constructor--
+# creating class **
+
+class student:
+     def __init__ (self,fullname):  #see here self means that object is calling itself 
+          self.name = fullname   
+
+
+# creating object(instance) **
+
+s2 = student("aditya")  #--() see this parentheses are use for constructor
+print(s2.name)
+
+# __________________________________________________________________________________________________
+# Note :-
+# see here the argument self or instead of it you can write abcd anything ,but its compulsary
+# to write that argumentotherwise you will have a error
+
+# The self parameter is a reference to the current instance of the class, and is used to access
+# variables that belongs to the class.
+# see here the self.name means, inside object it is going to create new
+# and like wise fullname here is the parameter in it.
+
+# see this type of constructor is called ---default constructor---
+# class student
+#      def __init__ (self)
+#       print("add database")
+# 
+# _______________________________________________________________________________________________
+# _______________________________________________________________________________________________
+#   this is a overview--       class
+#                               /\
+#                              /  \
+#                             /    \
+#                          data     Methods
+#                   (Attributes)    (functions)
+#     means properies in a way       means a function written in class
+
+
+# Class & Instance Attributes
+
+#  Class Attribute (SHARED)
+#  Meaning-----
+# A class attribute is shared by ALL objects of the class
+# Think: one common notice board 
+
+# class Student:
+#     college = "Zeal College"   # class attribute
+
+#     def __init__(self, name):
+#         self.name = name       # instance attribute
+
+# Here:
+# college belongs to the class
+# Every student has the same college
+
+# _______________________________________________________________________________________
+# ________________________________________________________________________________________
+# Instance Attribute (UNIQUE)
+#  Meaning --
+# Instance attributes belong to ONE object only
+# Think: personal ID card 
+# Example
+
+# self.name = name
+
+# name is different for every student
+# Stored inside each object
+
+
+# print(s1.name)   # Adi
+# print(s2.name)   # Rahul
+
+# -------------------------use------------------------------
+# Why we NEED both--
+
+# Use class attributes when:
+# -value is same for all
+# -constants
+# -settings
+# -shared data
+
+# Use instance attributes when:
+# -value differs per object
+# -user data
+# -state
+# -identity
+
+# class attr < object attr -- usuallyobject atr has more prefrence
+# this is in case if you named the variable same 
     
-    
+# ______________________________________________________________________________________________________
+# ______________________________________________________________________________________________________
+# Methods --
+# 
+# methods are the function that belongs to the objects.
+ 
 
 
